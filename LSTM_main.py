8356448b956393cfa020066828b09d6beca5b78d
weigebushiyao <505456072@qq.com>
2020-05-19 15:21:21 +0800

    Initial commit

---
+++ LSTM_main.py
@@ -0,0 +1,3 @@
+from model.LSTM.LSTM_process import LSTM_Model
+lstm = LSTM_Model(job_name='LSTM_Model',model_folder_name='418',model_name='418')
+lstm.train_lstm()
---
+++ start_py.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+nohup python3 /home/HFData_PitchSystem_LSTM_Model/xgb_main.py > xgb_model.log 2>&1 &
